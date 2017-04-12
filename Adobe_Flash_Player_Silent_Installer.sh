0f61cd02f7a9fe449e0d3434bdae44e03c050019
Geoff Repoli <geoffrepoli@gmail.com>
2017-04-12 16:50:39 -0400

    Update Adobe_Flash_Player_Silent_Installer.sh

--- Adobe_Flash_Player_Silent_Installer.sh
+++ Adobe_Flash_Player_Silent_Installer.sh
@@ -3,6 +3,9 @@
 # ADOBE FLASH PLAYER SILENT INSTALLER
 # github.com/geoffrepoli
 
+# Create UUID to safely perform `rm -f` on workstations
+UUID=$(uuidgen)
+
 # Direct download URL to flash installer dmg contains the current version number and thus changes with each new version.
 # Source URL to Adobe XML file that contains element with current version #
 SOURCE="http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pl.xml"
@@ -10,14 +13,14 @@ SOURCE="http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/ver
 # Extract current version element from XML, format for use in direct download URL
 VERSION=$(curl -s "$SOURCE" | grep -e "update version" | awk -F\" '{print $2}' | sed 's/,/./')
 DOWNLOADURL="https://fpdownload.adobe.com/get/flashplayer/pdc/${VERSION}/install_flash_player_osx.dmg"
-DOWNLOADPATH="/private/tmp/1.adobe-flash.dmg"
+DOWNLOADPATH="/private/tmp/${UUID}_adobe_flash_${VERSION}_installer.dmg"
 
 echo "Downloading Adobe Flash Player $VERSION Installer..."
 curl -s "$DOWNLOADURL" -o "$DOWNLOADPATH"
 
 echo "Installing Adobe Flash Player..."
 # Create a temp dir to mount disk image at
-TMPMOUNT=$(mktemp -d /private/tmp/1.adobe-flash-tmp.XXXX)
+TMPMOUNT=$(mktemp -d /private/tmp/"$UUID"_adobe_flash_"$VERSION"_tmpdir.XXXX)
 
 # Mount disk image silently
 hdiutil attach "$DOWNLOADPATH" -mountpoint "$TMPMOUNT" -nobrowse -noverify -noautoopen 1> /dev/null
@@ -29,9 +32,9 @@ INSTALLPKG=$(find "$TMPMOUNT" -type f -name "*Flash*.pkg")
 # when it see the word "error" in the output, even if the error message is unrelated/minor
 installer -dumplog -pkg "$INSTALLPKG" -target / 2> /dev/null
 
-echo "Cleaning up up temporary files"
 # Unmount disk image, delete tempdir and dmg
+echo "Cleaning up up temporary files"
 hdiutil detach "$TMPMOUNT" 1> /dev/null
-rm -rf /private/tmp/1.adobe-flash*
+rm -rf /private/tmp/"$UUID"*
 
 echo "Done"
