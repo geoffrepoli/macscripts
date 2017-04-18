7ed0ffad046815860dc8b958f5e721b826e0e705
Geoff Repoli <geoffrepoli@gmail.com>
2017-04-18 10:40:38 -0400

    minor cosmetic changes

--- Remove Saved Wireless Networks.sh
+++ Remove Saved Wireless Networks.sh
@@ -15,7 +15,7 @@ SSIDS=( "network1" "network2" )
 
 # Get list of all local accounts by returning list of all accounts with UID >= 499
 # while this is the most common user setup in Mac envs, tweak this to your environment's needs if necessary
-CUSERS=$(/usr/bin/dscl /Local/Default list /Users uid | awk '$2>=499{print $1}')
+LOCALUSERS=$(/usr/bin/dscl /Local/Default list /Users uid | awk '$2>=499{print $1}')
 
 # Get the hardware port that the Mac is currently using for Wi-Fi, as this can be different across Macs
 INTERFACE=$(/usr/sbin/networksetup -listallhardwareports | grep -A1 Wi-Fi | awk -F': ' '/Device/{print $NF}')
@@ -28,10 +28,11 @@ for SSID in "${SSIDS[@]}" ; do
     fi
 
     # Check each local user's login keychain for the associated 802.1X password entry and remove if found
-    for CUSER in "${CUSERS[@]}" ; do
+    for LOCALUSER in "${LOCALUSERS[@]}" ; do
     
-        if SSID="$SSID" su "$CUSER" -c '/usr/bin/security find-generic-password -s "com.apple.network.eap.user.item.wlan.ssid.$SSID" &> /dev/null' ; then 
-            SSID="$SSID" su "$CUSER" -c '/usr/bin/security delete-generic-password -s "com.apple.network.eap.user.item.wlan.ssid.$SSID" &> /dev/null' && echo "Removed keychain entry: $SSID - $CUSER"
+        if SSID="$SSID" su "$LOCALUSER" -c '/usr/bin/security find-generic-password -s "com.apple.network.eap.user.item.wlan.ssid.$SSID" &> /dev/null' ; then 
+            SSID="$SSID" su "$LOCALUSER" -c '/usr/bin/security delete-generic-password -s "com.apple.network.eap.user.item.wlan.ssid.$SSID" &> /dev/null' &&
+            echo "Removed keychain entry: $SSID - $LOCALUSER"
         fi
 
     done
