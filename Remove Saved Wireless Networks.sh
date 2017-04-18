3364489e55b3f6881ed041441287c8b6dbade1b9
Geoff Repoli <geoffrepoli@gmail.com>
2017-04-18 11:23:39 -0400

    Update Remove Saved Wireless Networks.sh

--- Remove Saved Wireless Networks.sh
+++ Remove Saved Wireless Networks.sh
@@ -8,7 +8,6 @@
 
 
 # ===============
-# SCRIPT CONFIGURATION
 # Add SSIDs to be removed in the following array, using the same formatting as the example networks:
 SSIDS=( "network1" "network2" )
 # ===============
@@ -20,22 +19,19 @@ LOCALUSERS=$(/usr/bin/dscl /Local/Default list /Users uid | awk '$2>=499{print $
 # Get the hardware port that the Mac is currently using for Wi-Fi, as this can be different across Macs
 INTERFACE=$(/usr/sbin/networksetup -listallhardwareports | grep -A1 Wi-Fi | awk -F': ' '/Device/{print $NF}')
 
-for SSID in "${SSIDS[@]}" ; do
+for SSID in "${SSIDS[@]}"; do
 
     # Check for each ssid and remove if found
-    if /usr/sbin/networksetup -listpreferredwirelessnetworks "$INTERFACE" | grep "\<${SSID}\>" &> /dev/null ; then
+    if /usr/sbin/networksetup -listpreferredwirelessnetworks "$INTERFACE" | grep "\<${SSID}\>" &> /dev/null; then
         /usr/sbin/networksetup -removepreferredwirelessnetwork "$INTERFACE" "$SSID" &> /dev/null && 
         echo "Removed SSID: $SSID"
     fi
 
     # Check each local user's login keychain for the associated 802.1X password entry and remove if found
-    for LOCALUSER in "${LOCALUSERS[@]}" ; do
-    
-        if SSID="$SSID" su "$LOCALUSER" -c '/usr/bin/security find-generic-password -s "com.apple.network.eap.user.item.wlan.ssid.$SSID" &> /dev/null' ; then 
+    for LOCALUSER in "${LOCALUSERS[@]}"; do
+        if SSID="$SSID" su "$LOCALUSER" -c '/usr/bin/security find-generic-password -s "com.apple.network.eap.user.item.wlan.ssid.$SSID" &> /dev/null'; then 
             SSID="$SSID" su "$LOCALUSER" -c '/usr/bin/security delete-generic-password -s "com.apple.network.eap.user.item.wlan.ssid.$SSID" &> /dev/null' &&
             echo "Removed keychain entry: $SSID - $LOCALUSER"
         fi
-
     done
-
 done
