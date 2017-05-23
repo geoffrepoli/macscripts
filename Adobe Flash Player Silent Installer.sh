4bcd688f309a39fb35d2037f6a757964d1a79f42
Geoff Repoli <geoffrepoli@users.noreply.github.com>
2017-05-23 13:10:40 -0400

    Update Adobe Flash Player Silent Installer.sh

--- Adobe Flash Player Silent Installer.sh
+++ Adobe Flash Player Silent Installer.sh
@@ -2,39 +2,39 @@
 #
 # ADOBE FLASH PLAYER SILENT INSTALLER
 # github.com/geoffrepoli
+# v2.0.0
+#################
+# Finds latest version of Adobe Flash Player, downloads and install silently. 
+# Cleans up install files before exiting.
 
-# Create UUID to safely perform `rm -f` on workstations
-UUID=$(/usr/bin/uuidgen)
+APPLICATION_NAME="Adobe Flash Player"
+APPLICATION_VERSION=$(/usr/bin/curl -s "http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pl.xml" | awk -F\" '/update version/{gsub(",",".");print $2}')
+APPLICATION_URL="https://fpdownload.adobe.com/get/flashplayer/pdc/${APPLICATION_VERSION}/install_flash_player_osx.dmg"
+SCRIPT_UUID=$(/usr/bin/uuidgen)
+DMG_PATH="/private/tmp/${SCRIPT_UUID}_${APPLICATION_NAME//\ /_}.dmg"
 
-# Direct download URL to flash installer dmg contains the current version number and thus changes with each new version.
-# Source URL to Adobe XML file that contains element with current version #
-SOURCE="http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pl.xml"
+echo "Downloading $APPLICATION_NAME v$APPLICATION_VERSION..."
+/usr/bin/curl -s "$APPLICATION_URL" -o "$DMG_PATH"
 
-# Extract current version element from XML, format for use in direct download URL
-VERSION=$(/usr/bin/curl -s "$SOURCE" | grep -e "update version" | awk -F\" '{print $2}' | sed 's/,/./')
-DOWNLOADURL="https://fpdownload.adobe.com/get/flashplayer/pdc/${VERSION}/install_flash_player_osx.dmg"
-DOWNLOADPATH="/private/tmp/${UUID}_adobe_flash_${VERSION}_installer.dmg"
+echo "Installing $APPLICATION_NAME..."
 
-echo "Downloading Adobe Flash Player $VERSION Installer..."
-/usr/bin/curl -s "$DOWNLOADURL" -o "$DOWNLOADPATH"
-
-echo "Installing Adobe Flash Player..."
 # Create a temp dir to mount disk image at
-TMPMOUNT=$(/usr/bin/mktemp -d /private/tmp/"$UUID"_adobe_flash_"$VERSION"_tmpdir.XXXX)
+TMP_MOUNT=$(/usr/bin/mktemp -d /private/tmp/${SCRIPT_UUID}_${APPLICATION_NAME//\ /_}_MOUNT.XXXX)
 
 # Mount disk image silently
-/usr/bin/hdiutil attach "$DOWNLOADPATH" -mountpoint "$TMPMOUNT" -nobrowse -noverify -noautoopen 1> /dev/null
+/usr/bin/hdiutil attach "$DMG_PATH" -mountpoint "$TMP_MOUNT" -nobrowse -noverify -noautoopen 1> /dev/null
 
 # Get path to installer pkg within mounted disk image
-INSTALLPKG=$(find "$TMPMOUNT" -type f -name "*Flash*.pkg")
+APPLICATION_PKG=$(find "$TMP_MOUNT" -type f -name "*Flash*.pkg")
 
 # install silently, redirect stderr to /dev/null to avoid jamf bug that reports the policy as failed
 # when it see the word "error" in the output, even if the error message is unrelated/minor
-/usr/sbin/installer -dumplog -pkg "$INSTALLPKG" -target / 2> /dev/null
+/usr/sbin/installer -dumplog -pkg "$APPLICATION_PKG" -target / 2> /dev/null
 
 # Unmount disk image, delete tempdir and dmg
-echo "Cleaning up up temporary files"
-/usr/bin/hdiutil detach "$TMPMOUNT" 1> /dev/null
-rm -rf /private/tmp/"$UUID"*
+echo "Cleaning up up temporary files..."
+/usr/bin/hdiutil detach "$TMP_MOUNT" 1> /dev/null
+rm -rf /private/tmp/"$SCRIPT_UUID"*
 
 echo "Done"
+exit 0
