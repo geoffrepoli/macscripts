1ef885ca095ad1cea349d100d891dc0e997f9816
doggles <8885886+doggles@users.noreply.github.com>
2017-08-29 22:49:53 -0400

    Update Remove Saved WPA2 Enterprise Wireless Networks.sh

--- Remove Saved WPA2 Enterprise Wireless Networks.sh
+++ Remove Saved WPA2 Enterprise Wireless Networks.sh
@@ -1,36 +1,36 @@
 #!/usr/bin/env bash
 #
+# ----
 # REMOVE SAVED WPA2 ENTERPRISE WIRELESS NETWORKS
-# github.com/geoffrepoli
-# v2.0
-# ===
+# v2.1 | github.com/doggles
+# ----
 # Removes SSID(s) from preferred network list & removes saved login credentials from each console user's keychain
 # Note: must be run as root
-# ===
+# ----
 
-# SCRIPT CONFIGURATION
+# CONFIGURATION
 # Add SSIDs to be removed in the following array, using the same formatting as the example networks:
 wirelessNetworks=( "wireless1" "wireless2" "wireless3" )
 
-#################
+# ------------
 
 # Get list of all local accounts by returning list of all accounts with UID >= 499
 # while this is the most common user setup in Mac envs, tweak this to your environment's needs if necessary
-localUsers=$(dscl . list /users uid | awk '$2>=499{print $1}')
-#
+localUsers=$(dscl . list /users uid | awk '$2 >= 499 {print $1}')
+
 # Get the hardware port that the Mac is currently using for Wi-Fi, as this can be different across Macs
 networkService=$(networksetup -listallhardwareports | awk -F': ' '/Wi-Fi/{getline;print $NF}')
 
 for ssid in ${wirelessNetworks[@]}; do
     # Check for each SSID and remove if found
-    if networksetup -listpreferredwirelessnetworks "$networkService" | grep -Fx "$ssid" &>/dev/null; then
-        networksetup -removepreferredwirelessnetwork "$networkService" "$ssid" &>/dev/null && 
+    if networksetup -listpreferredwirelessnetworks "$networkService" | grep -Fx "$ssid" &> /dev/null; then
+        networksetup -removepreferredwirelessnetwork "$networkService" "$ssid" &> /dev/null && 
         echo "Removed SSID: $ssid"
     fi
     # Check each local user's login keychain for the associated 802.1X password entry and remove if found
     for user in ${localUsers[@]}; do
-        if ssid="$ssid" su "$user" -c 'security find-generic-password -s "com.apple.network.eap.user.item.wlan.ssid.$ssid" &>/dev/null'; then 
-        	ssid="$ssid" su "$user" -c 'security delete-generic-password -s "com.apple.network.eap.user.item.wlan.ssid.$ssid" &>/dev/null' &&
+        if ssid="$ssid" su - "$user" -c 'security find-generic-password -s "com.apple.network.eap.user.item.wlan.ssid.$ssid" &> /dev/null'; then 
+        	ssid="$ssid" su - "$user" -c 'security delete-generic-password -s "com.apple.network.eap.user.item.wlan.ssid.$ssid" &> /dev/null' &&
         	echo "Removed keychain entry: $ssid - $user"
         fi
     done
